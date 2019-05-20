4a6165078b5a0e7537ae45de03514a5b059501d9
Zeal Zhu <731jerry@gmail.com>
2019-05-20 23:43:03 +0800

    update, a few bug need to be fixed due tmr

--- clipboard.py
+++ clipboard.py
@@ -10,20 +10,26 @@ def get_cb():
     cbstring = cb.stringForType_(NSStringPboardType)
     return cbstring
 
-def load_local_cb():
+def load_local_cb(filename):
     """
     载入本地json的clipboard
     """
-    pass
+    with open(filename, 'r') as json_file:
+        content = json_file.readline()
+        js = json.loads(content)
+        return js
 
-def save_cb_to_local():
+def save_cb_to_local(filename):
     """
+    先载入，再添加新的
     保存到本地json
     """
-    data = [{get_cb():str(dt.datetime.now())}] # 中文问题没有解决
-    # js  = json.dumps(data)
-    # print(js)
-    with open('data.json', 'w') as json_file:
+    data = load_local_cb(filename)
+    new_data = [{get_cb():str(dt.datetime.now())}] # 中文问题没有解决
+    data.append(new_data) # 格式有问题
+    js  = json.dumps(data)
+    print(js)
+    with open(filename, 'w') as json_file:
         json.dump(data, json_file)
 
 
@@ -37,6 +43,11 @@ def clear_cb_data():
     """
     清空clipboard的json文件内容
     """
-    pass
+    with open(filname, 'w') as json_file:
+        json_file.seek(0)
+        json_file.truncate()
 
-save_cb_to_local()
+
+save_cb_to_local('data.json')
+# load_local_cb('data.json')
+#clear_cb_data('data.json')
